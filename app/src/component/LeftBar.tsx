4faa1a0a19c0334214152bfd3028270ec9260bec
prachi <prachi@workplacefabric.com>
2024-02-13 22:31:32 +0530

    fontawesome and materialui setup

--- app/src/component/LeftBar.tsx
+++ app/src/component/LeftBar.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Button } from '@mui/material';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type Props = {}
 
 const LeftBar = (props: Props) => {
+
+  library.add(faCoffee);
     
   return (
-    <div>LeftBar</div>
+    <>
+      <div>LeftBar</div>
+      <Button variant='contained' color='primary'>
+      Hello from Material UI
+      </Button>
+      <FontAwesomeIcon icon="coffee" />
+    </>
   )
 }
 
